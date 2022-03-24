2a211dbc2e5432c4053bf7bed77c3d28bead53b1
Misael Calvillo Mancilla <misa@homely.mx>
2022-03-23 21:08:55 -0600

    Crear escena TaskList

--- src/App.js
+++ src/App.js
@@ -1,87 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { fetchTasks, createTask, deleteTask } from './api';
+import React from 'react';
+import TaskList from './scenes/TaskList';
 import './App.css';
 
-import Button from './components/Button';
-import Task from './components/Task';
-import TextInput from './components/TextInput/TextInput';
-
-
 function App() {
-  const [taskText, setTaskText] = useState("")
-  const [tasks, setTasks] = useState([])
-  const [loader, setLoader] = useState(false)
-
-  // console.count('App se renderiza')
-
-  useEffect(() => {
-    setLoader(true);
-    const timeoutId = setTimeout(() => {
-      console.log('Este proceso pasas despues de 5 segundos')
-      fetchTasks()
-        .then((res) => {
-          setTasks(res.data)
-          setLoader(false);
-        })
-        .catch((err) => {
-          console.error(err)
-        })
-    }, 5000)
-
-    return () => {
-      clearTimeout(timeoutId)
-    }
-  }, [])
-
-  const addTask = () => {
-    createTask(taskText)
-    .then((res) => {
-      const createdTask = res.data;
-      setTasks(tasks.concat(createdTask))
-      setTaskText('')
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-  }
-
-  const onDeleteTask = (id) => {
-    deleteTask(id)
-    .then((res) => {
-      const deltedTask = res.data
-      setTasks(tasks.filter((t) => t._id !== id))
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-  } 
-
   return (
     <div className="app">
       <header className="app-header">
-        <div className="task-input__container">
-          {/* // TODO: Hacer el componente TextInput */}
-            <TextInput 
-              value={taskText} 
-              placeholder="Ingresa una tarea"
-              type="text"
-              onChange={(e) => setTaskText(e.target.value)}   
-            />
-            <TextInput
-              value={taskText} 
-              placeholder="Ingresa tu contraseña"
-              type="password"
-              onChange={(e) => setTaskText(e.target.value)} 
-            />
-          <Button 
-            className="task-input__btn" 
-            onClick={addTask}
-          >Borrar tarea</Button>
-        </div>
-        {loader && (<p style={{ color: 'white' }}>Loading...</p>)}
-        {tasks.map((task) => (
-          <Task key={task._id} text={task.text} onDelete={() => onDeleteTask(task._id)} />
-        )).reverse()}
+        <TaskList />
       </header>
     </div>
   );
---
+++ src/scenes/TaskList.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { fetchTasks, createTask, deleteTask } from '../api';
+import './TaskList.css';
+import Button from '../components/Button';
+import Task from '../components/Task';
+import TextInput from '../components/TextInput/TextInput';
+
+function TaskList(props) {
+  const [taskText, setTaskText] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [loader, setLoader] = useState(false)
+
+  // console.count('App se renderiza')
+
+  useEffect(() => {
+    setLoader(true);
+    const timeoutId = setTimeout(() => {
+      console.log('Este proceso pasas despues de 5 segundos')
+      fetchTasks()
+        .then((res) => {
+          setTasks(res.data)
+          setLoader(false);
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }, 5000)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
+  const addTask = () => {
+    createTask(taskText)
+    .then((res) => {
+      const createdTask = res.data;
+      setTasks(tasks.concat(createdTask))
+      setTaskText('')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  const onDeleteTask = (id) => {
+    deleteTask(id)
+    .then((res) => {
+      const deltedTask = res.data
+      setTasks(tasks.filter((t) => t._id !== id))
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  } 
+
+  return (
+    <div className='task-list'>
+      <div className="task-input__container">
+        <TextInput 
+          value={taskText} 
+          placeholder="Ingresa una tarea"
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}   
+        />
+        <Button 
+          className="task-input__btn" 
+          onClick={addTask}
+        >Borrar tarea</Button>
+      </div>
+      {loader && (<p style={{ color: 'white' }}>Loading...</p>)}
+      {tasks.map((task) => (
+        <Task key={task._id} text={task.text} onDelete={() => onDeleteTask(task._id)} />
+      )).reverse()}
+    </div>
+  )
+}
+
+export default TaskList;
